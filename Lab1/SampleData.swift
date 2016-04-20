c159fb3eaee733b5dce2c50e97d1bfd9673d2644
Jason Lu <tcl012@ucsd.edu>
2016-04-20 11:41:29 -0700

    Lab 1 Assignment
    Thuan (Jason) Lu
    A09953686

---
+++ Lab1/SampleData.swift
@@ -0,0 +1,18 @@
+//
+//  SampleData.swift
+//  Lab1
+//
+//  Created by Jason Lu on 4/18/16.
+//  Copyright © 2016 Jason Lu. All rights reserved.
+//
+
+import Foundation
+
+//Set up sample data
+
+let contactsData = [
+    Contact(name:"Dr. Baker", phone:"(858) 333-3333", place: "School"),
+    Contact(name:"Joe Smarts", phone: "(858) 444-4444", place: "School"),
+    Contact(name:"Ma Besfrend", phone: "(858) 555-5555", place: "Friend"),
+    Contact(name:"Mom", phone: "(858) 666-6666", place: "Home"),
+    Contact(name:"Big Boss", phone: "(858) 777-7777", place: "Work")]
